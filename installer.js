c0e2a909dad6550edfac526c6e1103b6e510f735
Егорка <101581584+618-egorka@users.noreply.github.com>
2023-03-25 11:16:47 +0300

    Added error handling, dev-mode, mod ignore prefix

--- installer.js
+++ installer.js
@@ -5,8 +5,11 @@ const colors = require('colors');
 
 const modsLook = require('./data/mods/mods-look.json');
 const config = require('./config.json');
+const devMode = process.env.DEV_MODE || false;
+// const devMode = env.DEV_MODE === 'true' ? true : false;
 
 const modNamePrefix = config.modNamePrefix
+const modIngorePreifx = config.modIgnorePrefix
 
 const modsInstaller = fs.readdirSync(__dirname + '/data/mods');
 const modsInstallerPath = __dirname + '/data/mods';
@@ -24,36 +27,42 @@ main();
 
 function main(){
 	console.log('\x1Bc');
-	if(!minecraftRootFolderCheck()){
-		return;
+	try{
+		if(devMode){
+			message('devMode', 5000);
+		}
+		if(!minecraftRootFolderCheck()){
+			return;
+		}
+		if(!minecraftInnerFolderCheck('/mods', 'minecraftModsFolderDosentExist')){
+			return;
+		}
+		if(!minecraftInnerFolderCheck('/mods/old-mods', 'minecraftOldModsFolderDosentExist')){
+			return;
+		}
+		const folderData = getFolderData();
+		message('loading', 0);
+		modsPackageOperation(folderData);
+		oldModsMove(folderData);
+		message('installCompleted', 300000, modsLook.modsPackage.modsCoreVerison + ' | ' + modsLook.modsPackage.packageVersion);
+	} catch (err){
+		if (devMode){
+			console.error(err);
+		}
+		message('unknownError');
 	}
-	if(!minecraftModsFolderCheck()){
-		return;
-	}
-	if(!minecraftOldModsFolderCheck()){
-		return;
-	}
-	const folderData = getFolderData();
-	message('Loading', 0);
-	modsPackageOperation(folderData);
-	oldModsMove(folderData);
-	message('installCompleted', 300000, modsLook.modsPackage.modsCoreVerison + ' | ' + modsLook.modsPackage.packageVersion);
 }
 
 
 function getFolderData(){
-	let date = new Date();
+	const date = new Date();
 	const minecraftModsPath = minecraftPath + '/mods';
-	const modsMinecraft = fs.readdirSync(minecraftModsPath);
 	const oldModsPath =  minecraftModsPath + '/old-mods';
-	const oldModsPathDate = oldModsPath + '/' + date.toDateString().replace(/:/g, '-') + ' ' + date.getTime().toString(36);
-	console.log(minecraftModsPath);
-	console.log(fs.readdirSync(minecraftModsPath));
 	return {
 		minecraftModsPath: minecraftModsPath,
-		modsMinecraft: modsMinecraft,
+		modsMinecraft: fs.readdirSync(minecraftModsPath),
 		oldModsPath: oldModsPath,
-		oldModsPathDate: oldModsPathDate,
+		oldModsPathDate: oldModsPath + '/' + date.toDateString().replace(/:/g, '-') + ' ' + date.getTime().toString(36)
 	}
 }
 
@@ -66,23 +75,12 @@ function minecraftRootFolderCheck(){
 	return true;
 }
 
-function minecraftModsFolderCheck(){
-	if (!fs.existsSync(minecraftPath + '/mods')){
-		fs.mkdirSync(minecraftPath + '/mods');
+function minecraftInnerFolderCheck(innerPath, folderDosentExistMessageName){
+	if (!fs.existsSync(minecraftPath + innerPath)){
+		fs.mkdirSync(minecraftPath + innerPath);
 	}
-	if (!fs.existsSync(minecraftPath + '/mods')){
-		message('MinecraftModsFolderDosentExist');
-		return false;
-	}
-	return true;
-}
-
-function minecraftOldModsFolderCheck(){
-	if (!fs.existsSync(minecraftPath + '/mods/old-mods')){
-		fs.mkdirSync(minecraftPath + '/mods/old-mods');
-	}
-	if (!fs.existsSync(minecraftPath + '/mods/old-mods')){
-		message('MinecraftOldModsFolderDosentExist');
+	if (!fs.existsSync(minecraftPath + innerPath)){
+		message(folderDosentExistMessageName);
 		return false;
 	}
 	return true;
@@ -101,11 +99,10 @@ function modsPackageOperation(folderData){
 }
 
 function modsLookFilter(folderData){
-		console.log(folderData.modsMinecraft);
-		const modsMinecraftFiltered = folderData.modsMinecraft.filter(mod => mod.startsWith(modNamePrefix) && mod.endsWith('.jar')) || [];
+		const modsMinecraftFiltered = folderData.modsMinecraft.filter(mod => mod.startsWith(modNamePrefix) && mod.endsWith('.jar'));
 		const modsToDelete = modsMinecraftFiltered.filter(elem => modsLook.mods.findIndex(i=>i.name == elem) === -1);
 		modsToDelete.forEach(mod => {
-			fs.unlinkSync(folderData.minecraftModsPath + '/' + mod);
+		fs.unlinkSync(folderData.minecraftModsPath + '/' + mod);
 		});
 }
 
@@ -122,9 +119,12 @@ function actualModsLookCopy(folderData){
 }
 
 function oldModsMove(folderData){
+
 	let oldModsPathDateExist = fs.existsSync(folderData.oldModsPathDate);
 	for (const mod of folderData.modsMinecraft) {
-		if (!mod.startsWith(modNamePrefix) && mod.endsWith('.jar')) {
+		if (
+			!mod.startsWith(modNamePrefix) && mod.endsWith('.jar') &&
+			!mod.startsWith(modIngorePreifx) && mod.endsWith('.jar')) {
 			if (!oldModsPathDateExist) {
 				fs.mkdirSync(folderData.oldModsPathDate);
 				oldModsPathDateExist = true;
@@ -136,7 +136,9 @@ function oldModsMove(folderData){
 
 
 function message(name, timeMs = 300000, description = null){
-	console.log('\x1Bc');
+	if(!devMode){
+		console.log('\x1Bc');
+	}
 	if (description === null){
 		description = config.messages[name].description;
 	}
